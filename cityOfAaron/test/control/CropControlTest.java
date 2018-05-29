6769fa05adfbe6a8216c1f8e26d62a0d2c300ced
Aaron Sawyer <me@aarondevon.com>
2018-05-29 11:50:15 -0500

    Update CropControl Test.

--- cityOfAaron/test/control/CropControlTest.java
+++ cityOfAaron/test/control/CropControlTest.java
@@ -35,11 +35,38 @@ public class CropControlTest {
         
         System.out.println("\tTest case 1");
         CropData cropData = new CropData();
+        cropData.setPopulation(200);
+        cropData.setAcresOwned(1050);
+        cropData.setWheatInStore(2000);
         int landPrice = 20;
         int acresToBuy = 50;
         CropControl instance = new CropControl();
         int expResult = 1100;
         int result = CropControl.buyLand(landPrice, acresToBuy, cropData);
+        assertEquals(expResult, result, 0.0001);
+        
+        System.out.println("\tTest case 2");
+        // CropData cropData = new CropData();
+        cropData.setPopulation(200);
+        cropData.setAcresOwned(1000);
+        cropData.setWheatInStore(2700);
+        landPrice = 20;
+        acresToBuy = 50;
+        //CropControl instance = new CropControl();
+        expResult = 1050;
+        result = CropControl.buyLand(landPrice, acresToBuy, cropData);
+        assertEquals(expResult, result, 0.0001);
+        
+        System.out.println("\tTest case 3");
+        //CropData cropData = new CropData();
+        cropData.setPopulation(42);
+        cropData.setAcresOwned(682);
+        cropData.setWheatInStore(3422);
+        landPrice = 22;
+        acresToBuy = 20;
+        //CropControl instance = new CropControl();
+        expResult = -1;
+        result = CropControl.buyLand(landPrice, acresToBuy, cropData);
         assertEquals(expResult, result, 0.0001);
         // TODO review the generated test code and remove the default call to fail.
         //fail("The test case is a prototype.");
