e1e5d56c99191b04463ac9bc9ebd0411faf27da4
Carly Kjelstrom <winter.fox.studios@gmail.com>
2018-06-04 10:51:52 -0400

    added view package and MainMenuView class in package

---
+++ cityOfAaron/src/view/MainMenuView.java
@@ -0,0 +1,27 @@
+// The MainMenuView class - part of the view layer
+// Object of this class manages the main menu
+// Author: Aaron Sawyer, Carly Kjelstrom
+// Date last modified: May 2018
+//-------------------------------------------------------------
+package view;
+
+/**
+ *
+ * @author aaron and carly
+ */
+public class MainMenuView {
+    /**
+    * The displayMenuView method
+    * Purpose: displays the menu, gets the user's input, and does the
+    * selected action
+    * Parameters: none
+    * Returns: none
+    */
+    // =========================================================
+        public void displayMenuView() {
+        // Display the menu
+        // Prompt the user and get the user’s input
+        // Perform the desired action
+        // Determine and display the next view
+        }
+}
