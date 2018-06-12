2a9517d8801cef1943ba9cb5a4b6238fde928d0e
Aaron Sawyer <me@aarondevon.com>
2018-06-12 11:45:53 -0500

    Add sellLandView and feedPeopleView methods

--- cityOfAaron/src/view/CropView.java
+++ cityOfAaron/src/view/CropView.java
@@ -30,7 +30,7 @@ public class CropView {
         // Get the cost of land for this round.
         int price = CropControl.calcLandCost();
         // Prompt the user to enter the number of acres to buy
-        System.out.format("Land is selling for %d bushels per acre.%n",price);
+        System.out.format("You can buy land for %d bushels per acre.%n",price);
         System.out.print("How many acres of land do you wish to buy? ");
 
         // Get the user’s input and save it.
@@ -39,6 +39,34 @@ public class CropView {
         // Call the buyLand() method in the control layer to buy the land
         CropControl.buyLand(toBuy, price, cropData);
     }
+    
+    public static void sellLandView() {
+        // get the cost of land for this round
+        int price = CropControl.calcLandCost();
+        
+        // Prompt user to enter the number of acres to sell
+        System.out.format("You can sell land for %d bushels per acre.%n",price);
+        System.out.print("How many acres of land do you wish to sell? ");
+        
+        // Get the user’s input and save it.
+        int toSell;
+        toSell = keyboard.nextInt();
+        
+        // Call the sellLand() method in the control layer to sell the land
+        CropControl.sellLand(price, toSell, cropData);
+    }
+    
+    public static void feedPeopleView() {
+        // Prompt user to enter how many bushels of grain to give to the people
+        System.out.format("How many bushels of grain do you want to give to the people? ");
+        
+        // Get the user's input and save it.
+        int bushels = keyboard.nextInt();
+        
+        // Call the feedPeople() method in the control layer 
+        CropControl.feedPeople(bushels, cropData);
+    }
+    
     
     /**
     * The runCropsView method()
@@ -51,5 +79,5 @@ public class CropView {
         buyLandView();
         // add calls to the other crop view methods
         // as they are written
-    }   
+    }
 }
