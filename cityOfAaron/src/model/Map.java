0ca2df182f3b6b7c1915d907b6888556ad19f75b
Carly Kjelstrom <winter.fox.studios@gmail.com>
2018-06-26 09:37:32 -0400

    added map class and started

---
+++ cityOfAaron/src/model/Map.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author carlykjelstrom
+ */
+public class Map {
+    private int rowCount;               // stores the number of rows
+    private int colCount;               // stores the number of columns
+    private Location[] [] locations;    // a reference to a 2-dimensional array of Location objects
+}
